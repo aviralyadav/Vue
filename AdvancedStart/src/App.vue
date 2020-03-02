f9d5cedc1618726642265450e0d17ccc55efbe65
Aviral <aviral@Avirals-MacBook-Pro.local>
2020-03-02 23:00:40 +0530

    Slots

--- AdvancedStart/src/App.vue
+++ AdvancedStart/src/App.vue
@@ -2,16 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-quote>
+                   <h2 slot="title">The Quote</h2>
+                   <p>A Wonderful Quote!</p>
+               </app-quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+    import Quote from './components/Quote.vue';
     export default {
-        
+        components: {
+            appQuote: Quote
+        }
     }
 </script>
 
---
+++ AdvancedStart/src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <!-- <p>{{ quote }}</p> -->
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: {
+    //     quote: String
+    // }
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+    h2 {
+        color: red;
+    }
+    .title {
+        font-family: italic;
+    }
+</style>
