7676e07f52fed2a5e1602e3309a151bb478d7045
Aviral <aviral@Avirals-MacBook-Pro.local>
2020-03-08 13:43:35 +0530

    Quote App

---
+++ QuotesApp/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="addQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuotesGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data() {
+    return {
+      maxQuotes: 10,
+      quotes: ["this is just a quote", "Wow another one"]
+    };
+  },
+  methods: {
+    addQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes) {
+            return alert('You have to delete a Quote first!');
+        }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+</style>
