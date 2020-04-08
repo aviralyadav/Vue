71e7e293323427da9663515fc2cd30a86e5cc73c
Aviral <aviral@Avirals-MacBook-Pro.local>
2020-04-08 13:34:33 +0530

    added actions for async tasks because mutations are sync

--- Vuex/src/components/Counter.vue
+++ Vuex/src/components/Counter.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-primary" @click="increment(10)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(5)">Decrement</button>
+        <hr>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 100, duration: 2000})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 150, duration: 3000})">Decrement</button>
         <p>Vuex State Variable: {{status}}</p>
     </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from 'vuex';
     export default {
         methods: {
-            ...mapMutations(['increment', 'decrement'])
+            // ...mapMutations(['increment', 'decrement'])
+            ...mapActions([
+                'increment', 'decrement','asyncIncrement', 'asyncDecrement'
+            ])
         },
         computed: {
             ...mapGetters(['status'])
--- Vuex/src/store/store.js
+++ Vuex/src/store/store.js
@@ -10,18 +10,36 @@ export const store = new Vuex.Store({
     },
     getters: {
         doubleCounter: state => {
-            return state.counter * 2
+            return state.counter;
         },
         status: state => state.status
     },
-    mutations: {
-        increment: state => {
-            state.counter++;
+    mutations: { /// mutations are synchronous so for aync task or api calls we need to call mutations from actions
+        increment: (state, payload) => {
+            state.counter += payload;
             state.status = true;
         },
-        decrement: state => {
-            state.counter--;
+        decrement: (state, payload) => {
+            state.counter -= payload;
             state.status = false;
+        }
+    },
+    actions: { // actions are async
+        increment: ({commit}, payload) => {
+            commit('increment', payload);
+        },
+        decrement: ({commit}, payload) => {
+            commit('decrement', payload);
+        },
+        asyncIncrement: ({commit}, payload) => {
+            setTimeout(()=>{
+                commit('increment', payload.by)
+            } ,payload.duration);
+        },
+        asyncDecrement: ({commit}, payload) => {
+            setTimeout(()=>{
+                commit('decrement', payload.by)
+            } ,payload.duration);
         }
     }
 })
